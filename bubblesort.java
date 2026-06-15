79076c52dbac051ad6b30cd0277ca01a43095d67
sumit patil <spatil.0036@gmail.com>
2026-06-15 11:35:38 +0530

    bubble sort

---
+++ bubblesort.java
@@ -0,0 +1,25 @@
+public class bubblesort {
+
+    public static void bubble(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,3,4,1,7};
+        bubble(arr);
+        printarr(arr);
+    }
+}
